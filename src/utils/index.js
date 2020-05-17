acef95d0cbd54a239169766c788f2f4adbd7877a
Amber <wzhzzmzzy@foxmail.com>
2020-05-17 19:42:00 +0800

    fix: lint

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,23 +1,23 @@
-export function dateFormat(fmt, date) {
+export function dateFormat (fmt, date) {
   let ret
   const opt = {
-    "Y+": date.getFullYear().toString(),        // 年
-    "m+": (date.getMonth() + 1).toString(),     // 月
-    "d+": date.getDate().toString(),            // 日
-    "H+": date.getHours().toString(),           // 时
-    "M+": date.getMinutes().toString(),         // 分
-    "S+": date.getSeconds().toString()          // 秒
+    'Y+': date.getFullYear().toString(),
+    'm+': (date.getMonth() + 1).toString(),
+    'd+': date.getDate().toString(),
+    'H+': date.getHours().toString(),
+    'M+': date.getMinutes().toString(),
+    'S+': date.getSeconds().toString()
     // 有其他格式化字符需求可以继续添加，必须转化成字符串
   }
-  for (let k in opt) {
-    ret = new RegExp("(" + k + ")").exec(fmt);
+  for (const k in opt) {
+    ret = new RegExp('(' + k + ')').exec(fmt)
     if (ret) {
       fmt = fmt.replace(
         ret[1],
         ret[1].length === 1
           ? (opt[k])
-          : (opt[k].padStart(ret[1].length, "0")))
+          : (opt[k].padStart(ret[1].length, '0')))
     }
   }
-  return fmt;
+  return fmt
 }
